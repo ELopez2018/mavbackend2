6238e841e66ddd20b619bdec6ce74f1aa516b597
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2020-12-26 16:52:24 -0500

    Validacion

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,13 +1,23 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     public function login() {
-        return 'Logeo';
+       $response = $this->validate(request(), [
+            'email' => 'email|required|string',
+            'password' => 'required|string'
+        ]);
+
+
+        if(Auth::attempt($response))
+        {
+            return 'logeado';
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::get('/login',[LoginController::class, 'login'])->name('api.login');
+Route::post('/login',[LoginController::class, 'login'])->name('api.login');
