4494d359f14a8d97715e368eff2d16aa43623480
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2020-12-29 21:25:25 -0500

    Documentacion

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,20 +9,34 @@ use Illuminate\Support\Facades\Hash;
 class LoginController extends Controller
 {
     /**
-     * @api {post} http://miasesorvial3.test/api/v1/login Login de Usuarios
-     * @apiName Login
+     * @api {post} http://miasesorvial3.test/api/v1/users Registro de Usuarios
+     * @apiName login
      * @apiGroup Usuarios
      *
+     * @apiParam {String} name Nombre del usuario.
      * @apiParam {String} email Email del usuario es obligatorio.
      * @apiParam {String} password  Debe tener entre 6 y 20 caracteres.
-     * @apiSuccessExample {json} Respuesta de registro:
+     * @apiSuccessExample {json} Respuesta de logueo:
      *     HTTP/1.1 200 OK
-     *     {
-     *       "message": "Bienvendido al sistema",
-     *       "data": null,
-     *       "code": 200,
-     *       "details": null
-     *     }
+     *       {
+     *           "message": "Bienvendido al sistema",
+     *           "data": {
+     *               "user": {
+     *                   "id": 1,
+     *                   "role_id": 1,
+     *                   "name": "Estarlin",
+     *                   "email": "admin@admin.com",
+     *                   "avatar": "/storage/users/default.png",
+     *                   "email_verified_at": null,
+     *                   "settings": [],
+     *                   "created_at": "2020-12-28T20:17:52.000000Z",
+     *                   "updated_at": "2020-12-28T20:17:52.000000Z"
+     *                  },
+     *               "token": "X2TVWp@]jj)zVzfbZ2&ht8k!&C9F@#d9mdj]Uw]F.k[vrP6MH7i7jSafN3?n"
+     *           },
+     *           "code": 200,
+     *           "details": null
+     *       }
      */
     public function login(Request $request)
     {
